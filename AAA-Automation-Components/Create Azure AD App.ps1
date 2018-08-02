ae4b64fdc30e692900d0462936d1636990143317
A144395 <A144395@agl.com.au>
2018-08-02 23:46:12 +1000

    .

--- AAA-Automation-Components/Create Azure AD App.ps1
+++ AAA-Automation-Components/Create Azure AD App.ps1
@@ -2,9 +2,9 @@
 
 
 ## Create Azure AD Application for Automation
-#$SecureStringPassword = ConvertTo-SecureString -String $env:app_secret -AsPlainText -Force
+$SecureStringPassword = ConvertTo-SecureString -String "@gl@utomate" -AsPlainText -Force
 #$SecureStringPassword = Get-Content $env:app_secret | ConvertTo-SecureString
-$azureAdApplication = New-AzureRmADApplication -DisplayName "aaa.automation.app" -HomePage "http://aaaautomationapp" -IdentifierUris "http://aaaautomationapp" -Password "$env:app_secret"
+$azureAdApplication = New-AzureRmADApplication -DisplayName "aaa.automation.app" -HomePage "http://aaaautomationapp" -IdentifierUris "http://aaaautomationapp" -Password $SecureStringPassword
 $ServicePrincipal = New-AzureRmADServicePrincipal -ApplicationId $azureAdApplication.ApplicationId
 New-AzureRmRoleAssignment -ApplicationId $azureAdApplication.ApplicationId -RoleDefinitionName Contributor 
 #Remove-AzureRmADApplication -ApplicationId 0a27f4f2-258e-4d24-8d35-a1422e3fcfba
