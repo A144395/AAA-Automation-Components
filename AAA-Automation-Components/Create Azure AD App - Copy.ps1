e60c075d9daff647ddba227ff761a97547a3932c
A144395 <A144395@agl.com.au>
2018-08-02 16:17:54 +1000

    a

--- AAA-Automation-Components/Create Azure AD App - Copy.ps1
+++
@@ -1,6 +0,0 @@
-﻿## Create Azure AD Application for Automation
-$SecureStringPassword = ConvertTo-SecureString -String $emv:app_secret -AsPlainText -Force
-$azureAdApplication = New-AzureRmADApplication -DisplayName "aaa.automation.app" -HomePage "http://aaaautomationapp" -IdentifierUris "http://aaaautomationapp" -Password $SecureStringPassword
-$ServicePrincipal = New-AzureRmADServicePrincipal -ApplicationId $azureAdApplication.ApplicationId
-New-AzureRmRoleAssignment -ApplicationId $azureAdApplication.ApplicationId -RoleDefinitionName Contributor 
-#Remove-AzureRmADApplication -ApplicationId 0a27f4f2-258e-4d24-8d35-a1422e3fcfba
--- AAA-Automation-Components/Create Azure AD App.ps1
+++ AAA-Automation-Components/Create Azure AD App.ps1
@@ -1,18 +1,6 @@
 ﻿## Create Azure AD Application for Automation
-
-$SecureStringPassword = ConvertTo-SecureString -String "@gl@utomate" -AsPlainText -Force
+$SecureStringPassword = ConvertTo-SecureString -String $emv:app_secret -AsPlainText -Force
 $azureAdApplication = New-AzureRmADApplication -DisplayName "aaa.automation.app" -HomePage "http://aaaautomationapp" -IdentifierUris "http://aaaautomationapp" -Password $SecureStringPassword
-
-## Create Service Principal and link to Cert
-
 $ServicePrincipal = New-AzureRmADServicePrincipal -ApplicationId $azureAdApplication.ApplicationId
-
-#Wait for SP to be created
-
-Start-Sleep -Seconds 10
-
-##Assign Role to Azure AD App
-
 New-AzureRmRoleAssignment -ApplicationId $azureAdApplication.ApplicationId -RoleDefinitionName Contributor 
-
-#Remove-AzureRmADApplication -ApplicationId $azureAdApplication.ApplicationId
+#Remove-AzureRmADApplication -ApplicationId 0a27f4f2-258e-4d24-8d35-a1422e3fcfba
