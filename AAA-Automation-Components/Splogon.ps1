5051767a2a0b51a62dcd07587f805e124f5beba6
A144395 <A144395@agl.com.au>
2018-08-03 13:09:36 +1000

    .

--- AAA-Automation-Components/Splogon.ps1
+++ AAA-Automation-Components/Splogon.ps1
@@ -1,8 +1,12 @@
-﻿$secPassword = ConvertTo-SecureString “@gl@utomate” -AsPlainText –Force
-$clientid = "426c1c46-45df-4c9f-ac98-9786cd8f7531"
-$tenantid = "123913b9-915d-4d67-aaf9-ce327e8fc59f"
+﻿param(
+[string] $tenantid
+[string] $appid
+)
 
-$credential = New-Object System.Management.Automation.PSCredential($clientid, $secPassword)
 
+$secPassword = ConvertTo-SecureString “@gl@utomate” -AsPlainText –Force
+#$clientid = "426c1c46-45df-4c9f-ac98-9786cd8f7531"
+#$tenantid = "123913b9-915d-4d67-aaf9-ce327e8fc59f"
 
+$credential = New-Object System.Management.Automation.PSCredential($appid, $secPassword)
 Add-AzureRmAccount -Credential $credential -ServicePrincipal -Tenant $tenantid
