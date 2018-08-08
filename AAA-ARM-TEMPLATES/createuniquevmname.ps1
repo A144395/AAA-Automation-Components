ccb8fbc26548a0c0fc39315fe6f6806b6a57071d
A144395 <A144395@agl.com.au>
2018-08-08 12:44:25 +1000

    .

--- AAA-ARM-TEMPLATES/createuniquevmname.ps1
+++ AAA-ARM-TEMPLATES/createuniquevmname.ps1
@@ -12,20 +12,20 @@ Add-AzureRmAccount -Credential $credential -ServicePrincipal -Tenant $tenantid
 Select-AzureRmSubscription -SubscriptionName $sname
 
 
-$hname = '*azsapw*'
-$prefix = 'azsapw'
+$hname = '*azsaw*'
+$prefix = 'azsaw'
 $seqNo = 0000
 $lastVm = Get-AzureRmVM | Where-Object Name -Like $hname | Sort-Object Name -Descending | Select-Object -First(1) Name
 
 if($lastVm.Name -match "\d"){
     write-host "Hostname does not exists!"
-    $seqNo = ($lastVm.Name.Split('azsapw')) | Out-String 
+    $seqNo = ($lastVm.Name.Split('azsaw')) | Out-String 
     }
 
 $newNo = [int]($seqNo)+1
-$newNo = $newNo.ToString("00000")
+$newNo = $newNo.ToString("0000")
 $uniqueMcName = "$($prefix)$newNo"
 
 Write-Host New VM Name $uniqueMcName
-Write-Host "##vso[task.setvariable variable=vmname]$uniqueMcName"
+Write-Host ##vso[task.setvariable variable=vmname]$uniqueMcName"
 
